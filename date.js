1728d46c8bb4b26826c1341f09b4b6be130253cc
Md. Mahmodul Hassen Khan Shadin <shadhinkhan28@gmail.com>
2025-08-10 13:55:58 +0000

    update content

---
+++ date.js
@@ -0,0 +1,4 @@
+const today = new Date();
+const date = new Date('2025-11-19')
+console.log(date.getDay(), date.getMonth())
+
---
+++ math.js
@@ -0,0 +1,21 @@
+// const Math = {
+//     min : function min(num1,num2){
+//         return num2
+//     }
+// }
+
+// console.log(Math.min)
+// console.log(Math.max)
+
+const min = Math.min(45,1,56,123,21)
+console.log(min)
+console.log(Math.PI)
+console.log(Math.abs(-8+8))
+console.log(Math.floor(2.95))
+console.log(Math.floor(2.999555))
+console.log(Math.ceil(2.99995))
+console.log(Math.ceil(2.00001))
+console.log(Math.random(1,2))
+console.log(Math.random()*10)
+const rand = Math.round(Math.random()*10)
+console.log(rand)
