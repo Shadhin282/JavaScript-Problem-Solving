29e2566c3a1882403da5ae6b3fdd1903d3763fbb
Md. Mahmodul Hassen Khan Shadin <shadhinkhan28@gmail.com>
2025-08-12 03:53:56 +0000

    task 2

--- js_problem.js
+++ js_problem.js
@@ -9,3 +9,40 @@ function tempToCelsi(temp){
 
 let tempInFarhren = tempToCelsi(39);
 console.log(tempInFarhren);
+
+/* Task-2:
+You are given an array of numbers. Count how many times the a number is repeated in the array.
+
+sample-input: numbers = [5,6,11,12,98, 5]
+
+find: 5
+
+output: 2
+
+
+sample-input:
+
+numbers = [5,6,11,12,98, 5]
+
+find: 25
+
+output: 0
+*/
+
+function countNumber(numbers, findNum){
+   
+     let count = 0;
+    for(const num of numbers){
+        if(num === findNum){
+            count = count + 1;
+           
+        }
+}
+  return count;
+}
+let numbers =  [5,6,11,12,98, 5];
+let findNum = 25;
+console.log("Find : ", findNum);
+const countRepeatNum = countNumber(numbers, findNum);
+
+console.log("Count : ", countRepeatNum);
